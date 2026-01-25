25acf6d01f690f019170b69e2a77ef5ec1024ca5
Muskaan Mittal <muskaan@gl-login3.arc-ts.umich.edu>
2026-01-25 17:30:54 -0500

    label_and_partition script

---
+++ label_and_partition.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# NOTE: you can use this script to run LabelAndPartition as an sbatch job
+# $ sbatch label_and_partition.sh
+
+#SBATCH --job-name=label_and_partition
+#SBATCH --account=lsa2 # this might need to be changed, run $ my_accounts to see options
+#SBATCH --partition=gpu
+#SBATCH --time=00-06:00:00
+#SBATCH --mail-user=muskaan@umich.edu # use your own email here please!!
+#SBATCH --mail-type=END
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=5000m
+#SBATCH --gres=gpu:1
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+
+echo "________________________START________________________"
+
+python3 LabelAndPartition.py 2025v1_data.ndjson            # use appropriate year's .ndjson file here
+
+echo "________________________END________________________"
